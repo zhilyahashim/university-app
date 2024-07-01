40ccde39b023dae86a520a92aa4dbd628431f5d5
zhilyahashim <114016390+zhilyahashim@users.noreply.github.com>
2024-07-02 01:05:43 +0300

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,58 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 class StudentController extends Controller
 {
-    //
-    
-    // public function register(Request $req)
-    // {
-    //     $student = new Students();
-
-    //     $validatedData = $req->validate([
-    //         'student_name' => 'required|string',
-    //         'student_email' => 'required|email|unique:users,email',
-    //         'student_password' => 'required|string|min:8',
-    //         'student_phone' => 'required|nullable|regex:/^[0-9\- ]{10}$/|unique:users,phone',
-    //         'role' => 'required|string|in:user,student,headofdepartment,staff',
-    //     ]);
-        
-    //     $student->name = $validatedData['student_name'];
-    //     $student->email = $validatedData['student_email'];
-    //     $student->password = bcrypt($validatedData['student_password']);
-        
-    //     // Check if 'phone' key exists before accessing it
-    //     if (array_key_exists('student_phone', $validatedData)) {
-    //         $student->phone = $validatedData['student_phone'];
-    //     }
-        
-    //     if ($student->save()) {
-    //         return response()->json([
-    //             'success' => true,
-    //         ]);
-    //     } else {
-    //         return response()->json([
-    //             'success' => false,
-    //         ]);
-    //     }
-    //     $redirectRoute = '';
-    //     switch ($student->role) {
-    //         case 'student':
-    //             $redirectRoute = 'student.dashboard';
-    //             break;
-    //         case 'headofdepartment':
-    //             $redirectRoute = 'headofdepartment.dashboard';
-    //             break;
-    //         case 'staff':
-    //             $redirectRoute = 'staff.dashboard';
-    //             break;
-    //         default:
-    //             $redirectRoute = 'user.dashboard';
-    //             break;
-    //     }
-    
-    //     return redirect()->route($redirectRoute);
-        
-
-    // }
+   
 
     public function login(Request $req)
     {
